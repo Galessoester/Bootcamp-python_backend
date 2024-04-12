4a9820b26ee7964bba0885d27f9bc3364b0c1312
galessoester <galessoester@gmail.com>
2024-04-12 16:24:53 -0300

    variáveis e constantes

---
+++ Variaveis_e_constantes/variaveis_e_constantes.py
@@ -0,0 +1,15 @@
+# Definindo variáveis
+nome = 'Ester'
+idade = 25
+
+# Definindo variáveis na mesma linha
+nome, idade = 'Giovana', 28
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+# Definindo constantes
+STATES = ['SP', 'RJ', 'SC', 'RS']
+
+print(STATES)
+
