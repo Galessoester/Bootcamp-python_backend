a7480a776c6cece7544b2ed940a6c81277f1cba2
galessoester <galessoester@gmail.com>
2024-04-12 16:48:48 -0300

    Convertendo tipos

---
+++ Convertendo_tipos/convertendo_tipos.py
@@ -0,0 +1,17 @@
+# Float para Int
+print(int(10.2))
+
+# String para Int
+print(int('10'))
+
+# String para float
+print(float('10.2'))
+
+# Int para Float
+print(float(100))
+
+# Float para String
+print(str(10.2))
+
+# Int para String
+print(str(10))
