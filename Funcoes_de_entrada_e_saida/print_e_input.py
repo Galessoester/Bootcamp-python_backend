355e6223e194362015c4e3c7f90b425fbbef44e5
galessoester <galessoester@gmail.com>
2024-04-12 17:02:35 -0300

    print e input

---
+++ Funcoes_de_entrada_e_saida/print_e_input.py
@@ -0,0 +1,7 @@
+nome = input('Informe o seu nome: ')
+idade = input('Informe dua idade: ')
+
+
+print(f'Seu nome é {nome} e sua idade é {idade}')
+print(nome, idade, end = ' Fim\n')
+print(nome, idade, sep = '@')
