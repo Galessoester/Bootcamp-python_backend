0e2ac5e0c4f0fcfb330a25b628704c17daac3773
galessoester <galessoester@gmail.com>
2024-04-21 17:50:35 -0300

    Operadores de atribuição

---
+++ Dominando_Python_e_suas_estruturas_de_dados/TIpos_de_operadores_com_python/operadores_atribuicao.py
@@ -0,0 +1,37 @@
+# Atribuir
+saldo = 200
+print(saldo)
+
+# Sobrescrever
+saldo = 500
+print(saldo)
+
+# Somar
+saldo += 500 # saldo = saldo + 500
+print(saldo)
+
+# Subtrair
+saldo -= 20 # saldo = saldo - 20
+print(saldo)
+
+# Multiplicar
+saldo *= 5 # saldo = saldo * 5
+print(saldo)
+
+# Dividir (inteiro)
+saldo //= 5 # saldo = saldo // 5
+print(saldo)
+
+# Dividir
+saldo /= 2 # saldo = saldo / 2
+print(saldo)
+
+# Resto de divisão
+saldo %= 4 # saldo = saldo % 4
+print(saldo)
+
+# Potenciação
+saldo **= 2 # saldo = saldo ** 2
+print(saldo)
+
+
