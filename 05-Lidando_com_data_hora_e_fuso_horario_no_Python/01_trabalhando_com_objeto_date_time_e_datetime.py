f1e42981967d3ebf60f346b9cf3a31dc65c174d0
galessoester <galessoester@gmail.com>
2024-07-11 21:08:12 -0300

    Data e Hora

---
+++ 05-Lidando_com_data_hora_e_fuso_horario_no_Python/01_trabalhando_com_objeto_date_time_e_datetime.py
@@ -0,0 +1,13 @@
+from datetime import date, datetime, time
+
+data = date(2024, 7, 1)
+print(data)
+print(date.today())
+
+
+data_hora = datetime(2024, 7, 11)
+print(data_hora)
+print(datetime.today())
+
+hora = time(10, 20, 0)
+print(hora)
