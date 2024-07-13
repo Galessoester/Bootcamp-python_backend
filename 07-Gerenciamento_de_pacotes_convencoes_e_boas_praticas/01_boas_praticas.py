ff691e547907253be9479646583e110439360aff
galessoester <galessoester@gmail.com>
2024-07-13 13:08:21 -0300

    gerenciamento de pacotes, convenções e boas práticas

---
+++ 07-Gerenciamento_de_pacotes_convencoes_e_boas_praticas/01_boas_praticas.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+a = "python"
+
+b = "soncasifca isncoascnp ap ipanvsoe9vna apisncpao9s oooooa-sojca aoscnmpajca ipancpohawca painshcajwc piancaj9-jca"
+
+frutas = [
+    "maçã",
+    "banana",
+    "carambola",
+    "abacate",
+    "framboesa",
+    "melão",
+    "maracujá",
+    "melancia",
+    "morango",
+    "pera",
+    "acerola",
+]
+
+carros = ["celta", "fusca", "brasilia", "gol"]
